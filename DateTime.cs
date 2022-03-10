bb214d5c67e9cbcd0ed2a7dcea40a85e3960ba58
David <davidyujia@gmail.com>
2022-03-10 15:33:20 +0800

    add method ToUtcIso8061String

---
+++ DateTime.cs
@@ -0,0 +1,15 @@
+﻿namespace System;
+public static class DateTimeExtensions
+{
+    public static string ToUtcIso8061String(this DateTimeOffset dateTimeOffset)
+    {
+        return dateTimeOffset.ToString("u").Replace(" ", "T");
+    }
+
+    public static string ToUtcIso8061String(this DateTime dateTime)
+    {
+        DateTimeOffset dateTimeOffset = dateTime;
+
+        return dateTimeOffset.ToUtcIso8061String();
+    }
+}
